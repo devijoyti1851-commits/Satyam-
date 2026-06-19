b00cead00129937dc6483bffe205ac31c0d3f87d
devijoyti1851-commits <devijoyti1851@gmail.com>
2026-06-19 12:47:09 +0530

    Add EnemyHealth system for damage and death mechanics

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        Debug.Log("Enemy Health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy Defeated!");
+        Destroy(gameObject);
+    }
+}
